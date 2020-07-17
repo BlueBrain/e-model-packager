1e36ee29aa9f4e550ba447ba8192154043560af5
Anil Tuncel <anil.tuncel@epfl.ch>
2020-07-17 15:11:37 +0200

    initial attempt to CreateMeModelDirs in a task
    Change-Id: I7126f32b0befc285b53ab3cf95f7c14d092caff9

--- e_model_packages/sscx2020/build_packages.py
+++ e_model_packages/sscx2020/build_packages.py
@@ -4,7 +4,9 @@ import os
 import json
 import collections
 import luigi
-from utils import read_circuit, NpEncoder
+import shutil
+from luigi.contrib.simulate import RunAnywayTarget
+from utils import read_circuit, NpEncoder, combine_names
 from bluepy.v2 import Cell as bpcell
 
 
@@ -76,13 +78,136 @@ class SelectGids(luigi.Task):
             json.dump(mtype_etype_gids, out_file, indent=4, cls=NpEncoder)
 
 
-class SSCX2020(luigi.Task):
-    """The skeleton task to perform the workflow."""
+class CreateMEmodelDirs(luigi.Task):
+    """Creates me-model directories."""
+
+    @property
+    def config(self):
+        """Returns the Luigi config."""
+        return luigi.configuration.get_config()
 
     def requires(self):
-        """The SelectGids method is required."""
+        """ME type gids must be provided therefore SelectGids is required."""
         return SelectGids()
 
     def run(self):
-        """The starting point."""
-        print("SSCX2020 analysis is complete!")
+        """Create directories for each metype and cell combo."""
+        combo_tasks = []
+        with self.input().open("r") as metype_gids_file:
+            metype_gids = json.load(metype_gids_file)
+            for mtype in metype_gids.keys():
+                for etype in metype_gids[mtype]:
+                    gids = metype_gids[mtype][etype]
+                    for gid in gids:
+                        combo_tasks.append(CopyMechanisms(mtype, etype, gid))
+        self.output().done()
+        yield combo_tasks
+
+    def output(self):
+        return RunAnywayTarget(self)
+
+
+class CopyMechanisms(luigi.Task):
+    """Task to copy mechanisms to each memodel directory."""
+
+    mtype = luigi.Parameter()
+    etype = luigi.Parameter()
+    gid = luigi.IntParameter()
+
+    @property
+    def config(self):
+        """Returns the Luigi config."""
+        return luigi.configuration.get_config()
+
+    def requires(self):
+        """The folder structure must be created in advance."""
+        return CreateDir(self.mtype, self.etype, self.gid)
+
+    def output(self):
+        output_path = os.path.join(self.input().path, "mechanisms")
+        mecha_path = self.config.get("paths", "mechanisms")
+        mecha_files = self.config.get("mechanisms", "required_files")
+
+        output_files = []
+        for mecha_file in mecha_files:
+            output_files.append(os.path.join(mecha_path, mecha_file))
+
+        return output_files
+
+    def run(self):
+        output_path = os.path.join(self.input().path, "mechanisms")
+        mecha_path = self.config.get("paths", "mechanisms")
+        mecha_files = self.config.get("mechanisms", "required_files")
+
+        for mecha_file in mecha_files:
+            mecha_file_path = os.path.join(mecha_path, mecha_file)
+            shutil.copy(mecha_file_path, output_path)
+
+
+class CreateDir(luigi.Task):
+    """Task to create a model directory given mtype, etype and gid."""
+
+    mtype = luigi.Parameter()
+    etype = luigi.Parameter()
+    gid = luigi.IntParameter()
+
+    @property
+    def config(self):
+        """Returns the Luigi config."""
+        return luigi.configuration.get_config()
+
+    def run(self):
+        """Creates the nested directory."""
+        output_dir = self.config.get("paths", "output")
+        output_path = os.path.join(
+            output_dir,
+            "memodel_dirs",
+            self.mtype,
+            self.etype,
+            combine_names(self.mtype, self.etype, self.gid),
+        )
+        try:
+            os.makedirs(output_path)
+        # handled this way to have py2.7 support
+        except OSError:
+            pass
+
+    def output(self):
+        """The nested directory."""
+        output_dir = self.config.get("paths", "output")
+        output_path = os.path.join(
+            output_dir,
+            "memodel_dirs",
+            self.mtype,
+            self.etype,
+            combine_names(self.mtype, self.etype, self.gid),
+        )
+        return luigi.LocalTarget(output_path)
+
+
+class CopyMorphology(luigi.Task):
+    """Task to copy the morphology to each memodel directory."""
+    mtype = luigi.Parameter()
+    etype = luigi.Parameter()
+    gid = luigi.IntParameter()
+
+    def run(self):
+        raise NotImplementedError()
+
+
+class CopyScripts(luigi.Task):
+    """Task to copy scripts to each memodel directory."""
+    mtype = luigi.Parameter()
+    etype = luigi.Parameter()
+    gid = luigi.IntParameter()
+
+    def run(self):
+        raise NotImplementedError()
+
+
+class SSCX2020(luigi.WrapperTask):
+    """The skeleton task to perform the workflow."""
+
+    def requires(self):
+        """The CreateMEmodelDirs method is required."""
+        return CreateMEmodelDirs()
