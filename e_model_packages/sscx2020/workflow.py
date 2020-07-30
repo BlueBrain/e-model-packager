ce6f72aaaf67f2098242ad1c20e7da2e2b4ec510
Jaquier Aurélien Tristan <ajaquier@bbpv2.epfl.ch>
2020-07-30 17:59:28 +0200

    more style changes
    Change-Id: I76733e8a29704b8c02c679c317d1bcedde71f881

--- e_model_packages/sscx2020/workflow.py
+++ e_model_packages/sscx2020/workflow.py
@@ -103,8 +103,7 @@ class PrepareMEModelDirectory(luigi.Task):
 
     def copy_morph_emodel(
             self, circuit, blueconfig,
-            mecombo_emodels, memodel_dir, memodel_morph_dir
-            ):
+            mecombo_emodels, memodel_dir, memodel_morph_dir):
         """Copy morphology and emodel"""
         circ_morph_dir = os.path.join(blueconfig.Run["MorphologyPath"], "ascii")
         circ_emodel_dir = blueconfig.Run["METypePath"]
@@ -154,8 +153,7 @@ class PrepareMEModelDirectory(luigi.Task):
 
     def fill_in_templates(
             self, mecombo_thresholds, mecombo_hypamps, mecombo,
-            emodel, morph_fname, memodel_dir
-            ):
+            emodel, morph_fname, memodel_dir):
         """Fill in and write constants.hoc & current_amp.dat templates"""
         templates_dir = workflow_config.get("paths", "templates_dir")
 
